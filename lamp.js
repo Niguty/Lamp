143bb4ebd2bee72c587de64ee6d462ec31f4c5e9
Niguti <niguti005@gmail.com>
2024-12-06 08:40:03 -0300

    createElement

--- lamp.js
+++ lamp.js
@@ -4,4 +4,16 @@ class Lamp {
         this.y = initial.y
         this.turn = inital.turn
     }
+
+createElement(){
+    const lamp = document.createElement('div')
+        lamp.classList.add('lamp')
+    
+    const lampOff = document.createElement('div')
+        lampOff.classList.add('lampOff')
+    
+    const lampOn = document.createElement('div')
+        lampOn.classList.add('lampOn')
+    }
+    
 }
