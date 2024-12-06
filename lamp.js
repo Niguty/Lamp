f2ced5d4ae75c86c8ff39757744670be6a01ad4b
Niguti <niguti005@gmail.com>
2024-12-06 10:12:46 -0300

    luz acessa

--- lamp.js
+++ lamp.js
@@ -2,16 +2,24 @@ const button = document.getElementById('switchButton')
 const lamp = document.getElementById('lamp')
 
 let isLampOn = false;
+let click = false;
+
+lamp.addEventListener('click', () => {
+    if(click) {
+        lamp.src = 'images/lampQuebrada.png'
+    }
+})
 
 button.addEventListener('click', () => {
 
     if(isLampOn) {
-        opcao = (Math.random() * max);
-        lamp.src = 'luzApagada.png';
+        lamp.src = 'images/luzApagada.png';
         button.textContent = 'Switch'
+        document.body.style.backgroundColor = '#667';
     } else {
-        lamp.src = 'luzAcesa.png';
+        lamp.src = 'images/luzAcesa.png';
         button.textContent = 'Switch';
+        document.body.style.backgroundColor = 'yellow';
     }
 
     isLampOn = !isLampOn
