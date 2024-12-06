0cff6e3aeac3e497f0772c43dac9f7479f797ee8
Niguti <niguti005@gmail.com>
2024-12-06 09:02:11 -0300

    adaptacoes

--- lamp.js
+++ lamp.js
@@ -5,16 +5,4 @@ class Lamp {
         this.turn = inital.turn
     }
 
-createElement(){
-    const lamp = document.createElement('div')
-        lamp.classList.add('lamp')
-    
-    const lampOff = document.createElement('div')
-        lampOff.classList.add('lampOff')
-        lampOff.src = './app/luzApagada.png'
-        lampOff.alt = 'Luz Apagada'
-    
-    const lampOn = document.createElement('div')
-        lampOn.classList.add('lampOn')
-    }
 }
